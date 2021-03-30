4ad308646bd30267dcd79529e17fb79a5b817e6b
adfa <dlawo4339@khu.ac.kr>
2021-03-30 15:06:35 +0900

    [NEW] created EnableIf and IsIterator

---
+++ CaveEngine/Core/Public/Template/IsIterator.h
@@ -0,0 +1,21 @@
+/*!
+ * Copyright (c) 2021 SWTube. All rights reserved.
+ * Licensed under the GPL-3.0 License. See LICENSE file in the project root for license information.
+ */
+#pragma once
+#include "Containers/ContainersFwd.h"
+
+namespace cave
+{
+	template<typename Iterator>
+	class TIsIterator
+	{
+		static constexpr bool Value = false;
+	};
+
+	template<typename ContainerType, typename ElementType, typename SizeType>
+	class TIsIterator<TArrayIterator<ContainerType, ElementType, SizeType>>
+	{
+		static constexpr bool Value = true;
+	};
+}
