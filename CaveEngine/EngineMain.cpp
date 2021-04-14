61b40c5ecfd623bb4ad4f1927febd8b51f8068e8
alegruz <alegruz@khu.ac.kr>
2021-04-14 12:49:27 +0900

    [ADD] used gameplay team's tick manager

--- CaveEngine/EngineMain.cpp
+++ CaveEngine/EngineMain.cpp
@@ -3,6 +3,9 @@
  * Licensed under the GPL-3.0 License. See LICENSE file in the project root for license information.
  */
 #include "Graphics.h"
+#include "Time/TimeManager.h"
+
+#include <time.h>
 
 #if defined(__WIN32__)
 import Renderer;
@@ -22,6 +25,8 @@ int main(int argc, char** argv)
 int main()
 {
 #endif
+	cave::TimeManager timeManager;
+
     if (FAILED(cave::Renderer::Init("CaveEngineDemo")))
     {
         return 0;
@@ -43,10 +48,36 @@ int main()
         }
     }
 #elif defined(__UNIX__)
+	long double tic = 0.0l;
+	long double toc = 0.0l;
+	long double tick = 0.0l;
+	struct timespec tp;
+	long double startTic = 0.0l;
+	bool bHasSecondPassed = false;
+	uint32_t tickCount = 0u;
 	while (!cave::Renderer::GlfwWindowShouldClose())
 	{
+		clock_gettime(CLOCK_MONOTONIC, &tp);
+		tic = (static_cast<long double>(tp.tv_sec) * 1000.0l) + (static_cast<long double>(tp.tv_nsec) * 0.000001l);
+		if (bHasSecondPassed)
+		{
+			startTic = tic;
+			bHasSecondPassed = false;
+			LOGIF(cave::eLogChannel::GRAPHICS, std::cout, "FPS: %u", static_cast<uint32_t>(1.0l / ((tick / static_cast<long double>(tickCount)) * 0.001l)));
+			tick = 0.0l;
+			tickCount = 0u;
+		}
+		// LOGIF(cave::eLogChannel::GRAPHICS, std::cout, "Delta Time: %f", timeManager.GetDeltaTime());
 		cave::Renderer::Render();
 		glfwPollEvents();
+		clock_gettime(CLOCK_MONOTONIC, &tp);
+		toc = (static_cast<long double>(tp.tv_sec) * 1000.0l) + (static_cast<long double>(tp.tv_nsec) * 0.000001l);
+		tick += toc - tic;
+		++tickCount;
+		if (toc - startTic >= 1000.0l)
+		{
+			bHasSecondPassed = true;
+		}
 	}
 #endif
 
