606da841c7a17e3329ec0952038967b99c1a6544
박승준 <lo_rem7024@khu.ac.kr>
2021-07-20 17:53:50 +0900

    [CHANGE] changed methods about GameObject. [ADD] add variables and methods about GameObject.

--- CaveEngine/Gameplay/Private/World/Level.cpp
+++ CaveEngine/Gameplay/Private/World/Level.cpp
@@ -8,44 +8,135 @@
 
 namespace cave
 {
-	Level::Level(std::string& name) :
-		mName(name),
-		mMap(nullptr)
+	Level::Level(std::string& name) 
+		: Object(name)
+		, mMap(nullptr)
+		, mWorld(nullptr)
 	{
 
 	}
 
-	Level::Level(const char* name) :
-		mName(name),
-		mMap(nullptr)
+	Level::Level(const char* name)
+		: Object(name)
+		, mMap(nullptr)
+		, mWorld(nullptr)
 	{
 
 	}
 
-	void Level::SetName(std::string& name)
+	Level::~Level()
 	{
-		mName = std::move(name);
-	}
 
-	void Level::SetName(const char* name)
-	{
-		assert(name != nullptr);
-		mName = name;
 	}
 
 	void Level::AddGameObject(GameObject& gameObject)
 	{
-		mGameObjectsSortedByTag.insert({ gameObject.GetTag(), &gameObject });
+		assert(IsValid());
+		mAllGameObjects.insert({ gameObject.GetName(), &gameObject });
 		if (!gameObject.IsActive())
 		{
-			mDeactiveGameObjects.insert({ gameObject.GetName(), &gameObject });
 			return;
 		}
 		mActiveGameObjects.insert({ gameObject.GetName(), &gameObject });
 	}
 
+	void Level::AddGameObjects(std::vector<GameObject*>& gameObjects)
+	{
+		assert(IsValid());
+		for (auto iter = gameObjects.begin(); iter != gameObjects.end(); ++iter)
+		{
+			mAllGameObjects.insert({ (*iter)->GetName(), *iter });
+			if (!(*iter)->IsActive())
+			{
+				return;
+			}
+			mActiveGameObjects.insert({ (*iter)->GetName(), *iter });
+		}
+	}
+
 	void Level::RemoveGameObject(std::string& name)
 	{
-			
+
+	}
+
+	GameObject* Level::FindGameObjectByName(std::string& name)
+	{
+		assert(IsValid());
+		auto iter = mAllGameObjects.find(name);
+
+		return iter != mAllGameObjects.end() ? iter->second : nullptr;
+	}
+
+	GameObject* Level::FindGameObjectByName(const char* name)
+	{
+		assert(IsValid() && name != nullptr);
+		auto iter = mAllGameObjects.find(name);
+
+		return iter != mActiveGameObjects.end() ? iter->second : nullptr;
+	}
+
+	std::vector<GameObject*>&& Level::FindGameObjectsByName(std::string& name)
+	{
+		assert(IsValid());
+		std::vector<GameObject*> result;
+		auto range = mAllGameObjects.equal_range(name);
+		for (; range.first != range.second; ++range.first)
+		{
+			result.push_back(range.first->second);
+		}
+
+		return std::move(result);
+	}
+
+	GameObject* Level::FindGameObjectByTag(std::string& name)
+	{
+
+	}
+
+	GameObject* Level::FindGameObjectByTag(const char* name)
+	{
+
+	}
+
+	std::vector<GameObject*>&& Level::FindGameObjectsByTag(std::string& name)
+	{
+
+	}
+
+	std::vector<GameObject*>&& Level::FindGameObjectsByTag(const char* name)
+	{
+
+	}
+
+	std::vector<GameObject*>&& Level::FindGameObjectsByName(const char* name)
+	{
+		assert(IsValid());
+		assert(name != nullptr);
+		std::vector<GameObject*> result;
+		auto range = mAllGameObjects.equal_range(name);
+		for (; range.first != range.second; ++range.first)
+		{
+			result.push_back(range.first->second);
+		}
+
+		return std::move(result);
+	}
+	
+	void Level::UpdateGameObjectInLevel()
+	{
+		assert(IsValid());
+		for (auto iter = mActiveGameObjects.begin(); iter != mActiveGameObjects.end(); ++iter)
+		{
+			(iter->second)->UpdateScripts();
+		}
+	}
+
+	void Level::UpdateAllGameObjectInLevel()
+	{
+		assert(IsValid());
+		for (auto iter = mAllGameObjects.begin(); iter != mAllGameObjects.end(); ++iter)
+		{
+			(iter->second)->UpdateScripts();
+		}
 	}
 }
--- CaveEngine/Gameplay/Public/World/Level.h
+++ CaveEngine/Gameplay/Public/World/Level.h
@@ -7,15 +7,20 @@
 #include <vector>
 #include <unordered_map>
 
+#include "Object/Obejct.h"
+
 namespace cave
 {
+	class World;
 	class GameObject;
 	class Map;
 	class Tag;
 
-	class Level final
+	class Level final : public Object
 	{
 	public:
+		friend class GameObject;
+
 		Level() = delete;
 		Level(std::string& name);
 		Level(const char* name);
@@ -26,37 +31,46 @@ namespace cave
 		Level& operator=(const Level&) = delete;
 		Level& operator=(Level&&) = delete;
 
-		void SetName(std::string& name);
-		void SetName(const char* name);
-
 		void AddGameObject(GameObject& gameObject);
-		void AddGameObjects(std::vector<GameObject*> gameObjects);
+		void AddGameObjects(std::vector<GameObject*>& gameObjects);
 
 		void RemoveGameObject(std::string& name);
 		void RemoveGameObject(const char* name);
+		void RemoveGameObject(GameObject& gameObject);
 		void RemoveGameObjects(std::string& name);
-		void RemoveGameObjects(std::string& name);
+		void RemoveGameObjects(const char* name);
+		void RemoveGameObjects(std::vector<GameObject*>& gameObjects);
 
 		GameObject* FindGameObjectByName(std::string& name);
 		GameObject* FindGameObjectByName(const char* name);
-		std::vector<GameObject*>& FindGameObjectsByName(std::string& name);
-		std::vector<GameObject*>& FindGameObjectsByName(const char* name);
+		std::vector<GameObject*>&& FindGameObjectsByName(std::string& name);
+		std::vector<GameObject*>&& FindGameObjectsByName(const char* name);
 
 		GameObject* FindGameObjectByTag(std::string& tag);
 		GameObject* FindGameObjectByTag(const char* tag);
-		std::vector<GameObject*>& FindGameObjectsByTag(std::string& tag);
-		std::vector<GameObject*>& FindGameObjectsByTag(const char* tag);
+		std::vector<GameObject*>&& FindGameObjectsByTag(std::string& tag);
+		std::vector<GameObject*>&& FindGameObjectsByTag(const char* tag);
 
 		void UpdateGameObjectInLevel();
 		void UpdateAllGameObjectInLevel();
 
 	private:
-		std::unordered_multimap<std::string, GameObject*> mActiveGameObjects;
-		std::unordered_multimap<std::string, GameObject*> mDeactiveGameObjects;
-		/*Read only.*/
-		std::unordered_multimap<Tag*, GameObject*> mGameObjectsSortedByTag;
+		void addActiveGameObject(GameObject& gameObject);
+		void addActiveGameObjects(std::vector<GameObject*>& gameObjects);
 
-		std::string mName;
+		void removeActiveGameObject(GameObject& gameObject);
+		void removeActiveGameObjects(std::vector<GameObject*>& gameObjects);
+
+	private:
+		std::unordered_multimap<std::string, GameObject*> mAllGameObjects;
+		std::unordered_multimap<std::string, GameObject*> mActiveGameObjects;
+		
+		/*Read only.*/
+		std::unordered_multimap<Tag*, GameObject*> mAllTaggedGameObjects;
+		std::unordered_multimap<Tag*, GameObject*> mActiveTaggedGameObjects;
+
 		Map* mMap;
+
+		World* mWorld;
 	};
 }
