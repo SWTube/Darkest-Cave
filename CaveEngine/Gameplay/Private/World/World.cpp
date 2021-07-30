f5d81c18248203918a0c7f6c5f6d73d68f387c88
박승준 <lo_rem7024@khu.ac.kr>
2021-07-30 19:05:41 +0900

    [REMOVE] removed some method and variable. [NEW] Now World class inherit Object class.

--- CaveEngine/Gameplay/Private/World/World.cpp
+++ CaveEngine/Gameplay/Private/World/World.cpp
@@ -2,26 +2,73 @@
  * Copyright (c) 2021 SWTube. All rights reserved.
  * Licensed under the GPL-3.0 License. See LICENSE file in the project root for license information.
  */
+#include "Assertion/Assert.h"
+#include "World/Level.h"
 #include "World/World.h"
 
 namespace cave
 {
-	World::World(std::string& name) 
-		: mName(std::move(name))
-		, mCurrentLevel(nullptr)
-	{
+	std::unordered_set<std::string> World::mGlobalUniqueName;
 
+	World::World(std::string& name) 
+		: Object(name)
+	{
+		assert(IsValid() & mGlobalUniqueName.contains(name));
 	}
 
 	World::World(const char* name) 
-		: mName(name)
-		, mCurrentLevel(nullptr)
+		: Object(name)
 	{
-
+		assert(IsValid() & mGlobalUniqueName.contains(name));
 	}
 
 	World::~World()
 	{
+		assert(IsValid());
+	}
 
+	World::AddLevel(Level& level)
+	{
+		assert(IsValid() & level.IsValid());
+
+		mLevels.insert({ level.GetGUID(), &level });
+	}
+
+	World::AddLevels(std::vector<Level*>& levels)
+	{
+		for (auto& level : levels)
+		{
+			AddLevel(*level);
+		}
+	}
+
+	World::UpdateGameObjectsInWorld()
+	{
+		assert(IsValid());
+
+		for (auto& iter = mLevels.begin(); iter != mLevels.end(); ++iter)
+		{
+			iter->second->UpdateGameObjectsInLevel();
+		}
+	}
+
+	World::FixedUpdateGameObjectsInWorld()
+	{
+		assert(IsValid());
+
+		for (auto& iter = mLevels.begin(); iter != mLevels.end(); ++iter)
+		{
+			assert(iter->second->IsValid());
+			iter->second->UpdateGameObjectsInLevel();
+		}
+	}
+
+	World::isLevelInWorld(Level& level)
+	{
+		assert(level.IsValid());
+
+		auto iter = mLevels.find(level.GetGUID());
+
+		return iter->second != mLevels.end() ? true : false;
 	}
 }
