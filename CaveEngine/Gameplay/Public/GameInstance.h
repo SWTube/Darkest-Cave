a1eda345be740af44d465dfde32b78103853bd8d
박승준 <lo_rem7024@khu.ac.kr>
2021-07-30 20:43:31 +0900

    [CHANGE] changed Game class to GameInstance class.

---
+++ CaveEngine/Gameplay/Public/GameInstance.h
@@ -0,0 +1,40 @@
+/*!
+ * Copyright (c) 2021 SWTube. All rights reserved.
+ * Licensed under the GPL-3.0 License. See LICENSE file in the project root for license information.
+ */
+#pragma once
+
+#include <unordered_map>
+
+#include "CoreTypes.h"
+#include "GenericEngine.h"
+
+namespace cave
+{
+	class World;
+	class WorldMap;
+
+	class GameInstance final
+	{
+	public:
+		friend class GenericEngine;
+
+		GameInstance(const GameInstance&) = delete;
+		GameInstance(GameInstance&&) = delete;
+
+		~GameInstance();
+		GameInstance& operator=(const GameInstance&) = delete;
+		GameInstance& operator=(GameInstance&&) = delete;
+
+		void Init();
+		void Run();
+		void Destroy();
+
+	private:
+		GameInstance();
+
+	private:
+		std::unordered_map<uint32_t, World*> mWorlds;
+		WorldMap* mWorldMap;
+	};
+}
